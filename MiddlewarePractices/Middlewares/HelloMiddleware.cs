f77d83e48f7f16d42f4670c484d19cb41d069f72
Serhat OMAÇ <omacserhatt@gmail.com>
2022-10-15 20:37:48 +0300

    CustomExtensionMiddleware yaratıldı.

---
+++ MiddlewarePractices/Middlewares/HelloMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace MiddlewarePractices.Middlewares
+{
+    public class HelloMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public HelloMiddleware(RequestDelegate next)
+        {
+              _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("Hello World!");
+            await _next.Invoke(context);
+            Console.WriteLine("Bye World!");
+        }
+        public static class HelloMiddlewareExtension
+        {
+           public static IApplicationBuilder UseHello(IApplicationBuilder builder)
+            {
+                return builder.UseMiddleware<HelloMiddleware>();
+            }
+        }
+    }
+}
---
+++ MiddlewarePractices/Middlewares/HelloMiddlewareExtensionBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace MiddlewarePractices.Middlewares
+{
+    public static class HelloMiddlewareExtensionBase
+    {
+        public static IApplicationBuilder UseHello(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HelloMiddleware>();
+        }
+    }
+}
